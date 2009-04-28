5e3ab8b7cb56b7037bd0f27a6bbb991ae819cbdb
dustinbrett <dustinbrett@a90ab10a-1d43-11de-9909-f37ff7dbbc12>
2009-04-28 16:06:38 +0000

    Working on Info Idea
    git-svn-id: https://mpnzb.googlecode.com/svn/trunk@403 a90ab10a-1d43-11de-9909-f37ff7dbbc12

--- mpNZB/Clients/SABnzbd.cs
+++ mpNZB/Clients/SABnzbd.cs
@@ -217,7 +217,7 @@ namespace mpNZB.Clients
 
           foreach (XmlNode nodeItem in xmlDoc.SelectNodes("queue/jobs/job"))
           {
-            MP.ListItem(_List, nodeItem.SelectSingleNode("filename").InnerText, double.Parse(nodeItem.SelectSingleNode("mbleft").InnerText, nfi).ToString("N2") + " / " + double.Parse(nodeItem.SelectSingleNode("mb").InnerText, nfi).ToString("N2") + " MB", DateTime.Now, 0, nodeItem.SelectSingleNode("id").InnerText, 3);
+            MP.ListItem(_List, nodeItem.SelectSingleNode("filename").InnerText, double.Parse(nodeItem.SelectSingleNode("mbleft").InnerText, nfi).ToString("N2") + " / " + double.Parse(nodeItem.SelectSingleNode("mb").InnerText, nfi).ToString("N2") + " MB", DateTime.Now, 0, nodeItem.SelectSingleNode("id").InnerText, String.Empty, 3);
           }
 
           GUIPropertyManager.SetProperty("#Status", "Queue Loaded");
@@ -295,6 +295,7 @@ namespace mpNZB.Clients
       List<string> NZB = new List<string>();
 
       NZB.Add("Title: " + _Item.Label);
+      if (_Item.DVDLabel.Length > 0) { NZB.Add(_Item.DVDLabel); }
 
       if (MP.YesNo(NZB, "Download file?"))
       {
